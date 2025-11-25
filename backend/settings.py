0f044feb1a904802dce6cbaaf0012546c9da74dc
Feride <feride.kaya642@gmail.com>
2025-11-25 14:29:33 +0300

    Add CSRF_TRUSTED_ORIGINS and CORS_ALLOWED_ORIGINS

--- backend/settings.py
+++ backend/settings.py
@@ -121,19 +121,5 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.2/howto/static-files/
-
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# 2. Yüklenen resimlerin bilgisayarımızda fiziksel olarak nereye kaydedileceği.
-# (Proje ana klasörümüzün (backend) içinde 'media' adında bir klasör oluşturacak)
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-# backend/settings.py dosyasının en altına ekle
-
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
 }
