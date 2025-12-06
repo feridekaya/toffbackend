9354e45ddc1006fec8c5512a573179920fb89817
Feride <feride.kaya642@gmail.com>
2025-12-06 17:15:43 +0300

    Allow all CORS origins to fix Network Error

--- backend/settings.py
+++ backend/settings.py
@@ -134,7 +134,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOWED_ORIGINS = os.environ.get('CORS_ALLOWED_ORIGINS', 'http://localhost:3000,https://tofffrontend-production.up.railway.app').split(',')
+CORS_ALLOW_ALL_ORIGINS = True
 
 # CSRF Güvenlik Ayarları
 CSRF_TRUSTED_ORIGINS = [
